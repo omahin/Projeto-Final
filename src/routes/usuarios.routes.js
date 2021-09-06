9d3e369655b36b0c23b7bc460fbc24b0e533759b
Olga Mahin <olgamahin@hotmail.com>
2021-09-05 21:42:36 -0300

    Atualizei os códigos de autenticação

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,6 +4,6 @@ const controller = require('../controllers/usuariosControllers')
 
 router.post('/criar', controller.criarUsuario)
 
-router.get('/login', controller.login)
+router.post('/login', controller.login)
 
 module.exports = router
