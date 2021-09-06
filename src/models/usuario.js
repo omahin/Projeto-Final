e4905493ae2a9939f4f354714c470b8553ed4537
Olga Mahin <olgamahin@hotmail.com>
2021-09-05 21:26:12 -0300

    Atualizei os códigos de autenticação

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 
 const usuarioSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
     nome: {
         type: String,
         required: true
