b7035c4aa31caa835437841560b87d9fb6801b78
Olga Mahin <olgamahin@hotmail.com>
2021-09-09 01:16:08 -0300

    Atualizando as autenticações

--- src/controllers/terapeutaControllers.js
+++ src/controllers/terapeutaControllers.js
@@ -11,7 +11,22 @@ const todos = async (req, res) => {
     if (!token){
         return res.status(403).send({message: "Insira o token!"})
     }
-    jwt.verify(token == SECRETUSER && SECRETADM, async(err) => {
+    jwt.verify(token, SECRETUSER && SECRETADM, async(err) => {
+        if(err){
+            res.status(403).send({message: "Token não válido!", err})
+        }
+        const terapeutas = await Terapeuta.find()
+            res.status(200).json(terapeutas)
+    })
+}
+
+const todos = async (req, res) => {
+    const authHeader = req.get('authorization')
+    const token = authHeader.split(' ')[1]
+    if (!token){
+        return res.status(403).send({message: "Insira o token!"})
+    }
+    jwt.verify(token, SECRETADM, async(err) => {
         if(err){
             res.status(403).send({message: "Token não válido!", err})
         }
