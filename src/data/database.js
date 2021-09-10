50955b9c64b85d763ccd2da95bdd480a36943464
Olga Mahin <olgamahin@hotmail.com>
2021-09-10 03:12:56 -0300

    Tentando corrigir o código

--- src/data/database.js
+++ src/data/database.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 require('dotenv-safe').config()
 const mongoose = require('mongoose')
 
-const MONGO_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/procura-psi'
+const MONGODB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/procura-psi'
 
-const connect = () => {mongoose.connect(MONGO_URL, {
+const connect = () => {mongoose.connect(MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
